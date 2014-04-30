f0d59e518c4d90cf82e7b0c684a46b7a7e062ef9
jon5477 <huangjonathan@live.com>
2014-04-30 16:12:24 -0600

    removed unused controller

--- app/controllers/login_controller.rb
+++
@@ -1,9 +0,0 @@
-class LoginController < ApplicationController
-	def index
-		# check if the password is correct
-		# if the password is correct
-		# redirect to newsfeed
-		# else
-		# redirect to main page with error message
-	end
-end
