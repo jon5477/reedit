be6db7d6d904182ea88960fedcac7fa3dd691dd1
jon5477 <huangjonathan@live.com>
2014-05-05 04:17:38 -0600

    added posting

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,5 +1,18 @@
 class PostController < ApplicationController
 	def create
-		
+		if current_user != nil
+			@post = Post.new(post_params)
+			@post.user_id = current_user.id
+			@post.save
+			redirect_to current_user
+		else
+			redirect_to '/'
+		end
+	end
+
+	private
+
+	def post_params
+		params.require(:post).permit(:content)
 	end
 end
